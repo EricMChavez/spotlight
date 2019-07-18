ec2fc3d553e09c5c1bb8cf15ceb6a98ded545fea
Eric Chavez <eric@emcaudio.com>
2019-07-18 18:54:17 -0500

    Testing CORS Disable

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
-import pdf from './test.pdf';
+let pdf = 'https://spotlight-storage.s3.amazonaws.com/003-10-07-html-5-scalable-vector-graphics.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 

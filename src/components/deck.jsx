313d62ca3ef637387e84c08d5832ddc227184e89
Eric Chavez <eric@emcaudio.com>
2019-07-18 18:27:30 -0500

    testing URL to PDF

--- src/components/deck.jsx
+++ src/components/deck.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import Slide from './slide';
 class Deck extends Component {
 	render() {
-		return <div className="deck">SlideDeck</div>;
+		return (
+			<div className="deck">
+				<Slide />
+			</div>
+		);
 	}
 }
 
--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-class Display extends Component {
+import { Document, Page } from 'react-pdf';
+import { pdfjs } from 'react-pdf';
+import $ from 'jquery';
+let pdf = 'https://spotlight-storage.s3.amazonaws.com/003-10-07-html-5-scalable-vector-graphics.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+export default class Display extends Component {
 	render() {
-		return <div className="display">Display</div>;
+		return (
+			<Document className="display" file={pdf}>
+				<Page height={window.innerHeight - 150} className="page" pageNumber={1} />
+			</Document>
+		);
 	}
 }
-
-export default Display;

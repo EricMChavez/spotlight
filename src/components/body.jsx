1e292e59c099527de134f8125a0329287f3eec54
Eric Chavez <eric@emcaudio.com>
2019-07-18 19:41:58 -0500

    working slides

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -3,14 +3,26 @@ import Header from './header';
 import Deck from './deck';
 import Toolbar from './toolbar';
 import Display from './display';
+let pdf = 'https://spotlight-storage.s3.amazonaws.com/003-10-07-html-5-scalable-vector-graphics.pdf';
 class Body extends Component {
+	state = {
+		displayPage: 1
+	};
+	handleNext = () => {
+		console.log('next called');
+		this.setState({ displayPage: this.state.displayPage + 1 });
+	};
+	handleBack = () => {
+		console.log('back called');
+		this.setState({ displayPage: this.state.displayPage - 1 });
+	};
 	render() {
 		return (
 			<React.Fragment>
 				<Header />
-				<Deck />
-				<Toolbar />
-				<Display />
+				<Deck content={pdf} PageNumber={this.displayPage} />
+				<Toolbar onNext={this.handleNext} onBack={this.handleBack} />
+				<Display onClick={this.next} content={pdf} pageNumber={this.state.displayPage} />
 			</React.Fragment>
 		);
 	}
--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 class Toolbar extends Component {
 	render() {
-		return <div className="toolbar">Toolbar</div>;
+		return (
+			<div className="toolbar">
+				<button onClick={this.props.onBack}>Back</button>
+				<button onClick={this.props.onNext}>Next</button>
+			</div>
+		);
 	}
 }
 
